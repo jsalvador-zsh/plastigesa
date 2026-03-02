3f7724dfbf0c45c3d6b41ea7929314f514dc0d2b
jsalvador-zsh <juanstdev@gmail.com>
2026-03-02 09:41:09 -0500

    [ADD] Widget chatwoot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,21 +40,22 @@ export default async function RootLayout({
           dangerouslySetInnerHTML={{
             __html: `
               (function(d,t) {
-                  var BASE_URL="https://rq0yds.easypanel.host";
-                  var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-                  g.src=BASE_URL+"/packs/js/sdk.js";
-                  g.async = true;
-                  s.parentNode.insertBefore(g,s);
-                  g.onload=function(){
-                    window.chatwootSDK.run({
-                      websiteToken: 'av7aQB5Gg1mv3Gr4zgHWa8fR',
-                      baseUrl: BASE_URL
-                    })
-                  }
-                })(document,"script");
+                var BASE_URL="https://chat.plastigesa.org";
+                var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+                g.src=BASE_URL+"/packs/js/sdk.js";
+                g.async = true;
+                s.parentNode.insertBefore(g,s);
+                g.onload=function(){
+                  window.chatwootSDK.run({
+                    websiteToken: 'tjAMFgBHPhMwoZHXTfXGAKFv',
+                    baseUrl: BASE_URL
+                  })
+                }
+              })(document,"script");
             `
           }}
         />
+        
       </body>
     </html>
   )
